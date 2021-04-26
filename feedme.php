12d0421f6a1c4aebae2c04dcde0d8f5af4f3f12d
David Kjellson <david.kjellson@gmail.com>
2021-04-26 10:24:39 +0200

    I used too many arguments in randomword() (it still doesn't work)

--- feedme.php
+++ feedme.php
@@ -5,9 +5,18 @@ error_reporting(E_ALL);
 include 'db.php';
 $connection = new Db();
 for ($i = 0; $i < 100; $i++) {
-  $stmt = $connection->pdo->prepare("INSERT INTO  products (name, price, description, image) VALUES(':name:', ':price:', ':decription:', ':image:')");
-  $image = 'https://picsum.photos/seed/' . $i . '/200/300';
-  $stmt->execute([randomword(rand(4, 12), rand(10, 1000), rand(4, 12), $image)]);
+  try {
+    $stmt = $connection->pdo->prepare("INSERT INTO  products (name, price, description, image) VALUES(':name:', ':price:', ':decription:', ':image:')");
+    $image = 'https://picsum.photos/seed/' . $i . '/200/300';
+    $stmt->execute([
+      randomword(rand(4, 12)),
+      rand(10, 1000),
+      randomword(rand(4, 12)),
+      $image
+    ]);
+  } catch (Exception $e) {
+    echo "Connection failed:" . $e->getMessage();
+  }
 }
 function randomword($length = 4)
 {
