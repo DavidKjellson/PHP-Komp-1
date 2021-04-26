18bca0f9ce947af1e71f13af397971b2c49f68cf
David Kjellson <david.kjellson@gmail.com>
2021-04-26 09:46:02 +0200

    A way to add into database without doing it manually (that doesn't work)

---
+++ feedme.php
@@ -0,0 +1,15 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+include 'db.php';
+$connection = new Db();
+for ($i = 0; $i < 100; $i++) {
+  $stmt = $connection->pdo->prepare("INSERT INTO  products (name, price, description, image) VALUES(':name:', ':price:', ':decription:', ':image:')");
+  $image = 'https://picsum.photos/seed/' . $i . '/200/300';
+  $stmt->execute([randomword(rand(4, 12), rand(10, 1000), rand(4, 12), $image)]);
+}
+function randomword($length = 4)
+{
+  return substr(str_shuffle("qwertyuiopasdfghjklzxcvbnm"), 0, $length);
+}
